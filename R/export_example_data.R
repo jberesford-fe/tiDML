8d749f362ab69d2c280971cfde5dce7a7f76e926
jberesford-fe <Justin.Beresford@frontier-economics.com>
2024-12-03 11:14:04 +0000

    rename function for eg data

--- R/export_example_data.R
+++ R/export_example_data.R
@@ -4,10 +4,10 @@
 #'
 #' @return A dataframe with simulated data for DML.
 #' @examples
-#' df <- tiDML::example_data()
+#' df <- tiDML::get_example_data()
 #' head(df)
 #' @export
-example_data <- function() {
+get_example_data <- function() {
   data("example_data", package = "tiDML")
   example_data
 }
