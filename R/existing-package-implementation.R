2fbd4bbaa157cbc1531b385dd9c499d28ff276ee
Justin Beresford <Justin.Beresford@frontier-economics.com>
2025-07-14 19:18:48 +0100

    caret conf mat

--- R/existing-package-implementation.R
+++ R/existing-package-implementation.R
@@ -5,8 +5,22 @@ library(paradox)
 library(partykit)
 library(rpart)
 library(partykit)
+library(caret)
 
-df <- load_example_data()
+
+set.seed(42)
+
+n <- 500
+df <- data.frame(
+  X1 = rnorm(n),
+  X2 = rnorm(n),
+  D = rbinom(n, 1, 0.5)
+)
+linpred <- -0.3 + 0.7 * df$X1 - 1.2 * df$X2 + 0.5 * df$D
+p <- plogis(linpred)
+df$Y <- rbinom(n, 1, p)
+
+
 dml_df <- DoubleML::DoubleMLData$new(
   df,
   y_col = "Y",
@@ -14,8 +28,12 @@ dml_df <- DoubleML::DoubleMLData$new(
   x_cols = c("X1", "X2")
 )
 
-learner <- lrn("regr.rpart", cp = 0.001, keep_model = TRUE)
-
+learner <- lrn(
+  "classif.rpart",
+  predict_type = "prob",
+  cp = 0.001,
+  keep_model = TRUE
+)
 
 dml_plr <- DoubleMLPLR$new(
   data = dml_df,
@@ -23,29 +41,32 @@ dml_plr <- DoubleMLPLR$new(
   ml_m = learner$clone()
 )
 
-
-dml_plr$models$ml_l
-
 dml_plr$fit(store_predictions = TRUE, store_models = TRUE)
 
 model_l <- dml_plr$models$ml_l
 model_m <- dml_plr$models$ml_m
 
-
-rf_y <- dml_plr$models$ml_l[[1]][[1]][[1]]$model
+rf_y <- model_l[[1]][[1]][[1]]$model
 rf_y$variable.importance
 
+as.party(rf_y, data = TRUE) |>
+  plot(main = "Regression Tree for Y")
 
-library(rpart.plot)
-rpart.plot(
-  rf_y,
-  type = 3,
-  fallen.leaves = TRUE,
-  roundint = FALSE,
-  cex = 1.0, # 20 % larger text and boxes
-  branch.lwd = 2 # thicker lines
+
+preds_prob <- predict(rf_y, newdata = df)
+preds_class <- ifelse(preds_prob > 0.5, 1, 0)
+table(Predicted = preds_class, Actual = df$Y)
+
+caret::confusionMatrix(
+  factor(preds_class, levels = c(0, 1)),
+  factor(df$Y, levels = c(0, 1))
 )
 
 
-party_tree <- as.party(rf_y, data = TRUE)
-plot(party_tree, main = "Regression Tree for Y")
+rf_d <- dml_plr$models$ml_m[[1]][[1]][[1]]$model
+preds_d_prob <- predict(rf_d, newdata = df)
+preds_d_class <- ifelse(preds_d_prob > 0.5, 1, 0)
+confusionMatrix(
+  factor(preds_d_class, levels = c(0, 1)),
+  factor(df$D, levels = c(0, 1))
+)
