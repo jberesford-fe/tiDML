176bb51f22f9dbabb16140dfbc0fcc04482d6693
Justin Beresford <Justin.Beresford@frontier-economics.com>
2025-07-15 10:44:20 +0100

    make function for feature importance

--- R/existing-package-implementation.R
+++ R/existing-package-implementation.R
@@ -6,10 +6,15 @@ library(partykit)
 library(rpart)
 library(partykit)
 library(caret)
-
+library(ggplot2)
+library(stringr)
+library(dplyr)
+library(tidyr)
+library(purrr)
 
 set.seed(42)
 
+####### Make some synthetic (binary categorical) data #######
 n <- 500
 df <- data.frame(
   X1 = rnorm(n),
@@ -21,6 +26,14 @@ p <- plogis(linpred)
 df$Y <- rbinom(n, 1, p)
 
 
+df |>
+  ggplot(aes(x = X1, y = X2, color = factor(D))) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, aes(group = D)) +
+  labs(title = "Scatter plot of the synthetic data") +
+  theme_minimal()
+
+####### Set up DoubleML data structure #######
 dml_df <- DoubleML::DoubleMLData$new(
   df,
   y_col = "Y",
@@ -28,23 +41,87 @@ dml_df <- DoubleML::DoubleMLData$new(
   x_cols = c("X1", "X2")
 )
 
-learner <- lrn(
-  "classif.rpart",
-  predict_type = "prob",
-  cp = 0.001,
+
+####### Explore mlr3 learners #######
+
+# There are 15 classification learners
+as.data.table(mlr_learners) |>
+  as_tibble() |>
+  filter(task_type == "classif") |>
+  select(-predict_types) |>
+  unnest_wider(properties, names_sep = "_")
+
+# And 12 regression learners
+as.data.table(mlr_learners) |>
+  as_tibble() |>
+  filter(task_type == "regr") |>
+  select(-predict_types) |>
+  unnest_wider(properties, names_sep = "_")
+
+
+###### Set up one of the classification learners #######
+as.data.table(mlr_learners) |>
+  as_tibble() |>
+  filter(key == "regr.ranger") |>
+  unnest_wider(predict_types, names_sep = "_")
+
+decision_tree_learner <- lrn(
+  "regr.rpart",
+  predict_type = "response",
   keep_model = TRUE
 )
 
+
+random_forest_learner <- lrn(
+  "regr.ranger",
+  predict_type = "response",
+  num.trees = 3,
+  min.node.size = 4,
+  importance = "impurity"
+)
+
+####### Fit DoubleML PLR model #######
+
 dml_plr <- DoubleMLPLR$new(
   data = dml_df,
-  ml_l = learner$clone(),
-  ml_m = learner$clone()
+  ml_l = random_forest_learner$clone(),
+  ml_m = random_forest_learner$clone()
 )
 
 dml_plr$fit(store_predictions = TRUE, store_models = TRUE)
 
-model_l <- dml_plr$models$ml_l
+
+######### Check results for phase 1 model (estimate of D) #######
+
+# Check out the fold-level results
+# For the first fold (of 5) and repetition (of 1). There's only 1 treatment var.
 model_m <- dml_plr$models$ml_m
+
+rep <- 1
+treatment <- 1
+fold <- 3
+
+# Print OOB R^2 to the console - how do I pull it out?
+model_m[[rep]][[treatment]][[fold]]$model
+
+# Variable/feature importance for the first fold and repetition
+model_m[[rep]][[treatment]][[fold]]$importance()
+
+
+model_m[[1]][[1]][[5]]$model
+
+######## Extract overall model results ########
+# Now we know how to get them for single folds, let's get the overall results
+
+get_feature_importance <- function(model, rep, treatment, fold) {
+  model[[rep]][[treatment]][[fold]]$importance()
+}
+
+get_feature_importance(model_m, rep = 1, treatment = 1, fold = 1)
+
+folds <- 1:length(model_m[[1]][[1]])
+map_dfr(folds, ~ get_feature_importance(model_m, rep = 1, treatment = 1, .x))
+
 
 rf_y <- model_l[[1]][[1]][[1]]$model
 rf_y$variable.importance
